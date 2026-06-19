5d1e0f10026fd38ad805ff72879b1e42373e7e5f
Shaamil777 <shamilm2324@gmail.com>
2026-06-19 15:32:24 +0530

    feat:added GitHub Actions automation

--- src/sheets/sheetsClient.ts
+++ src/sheets/sheetsClient.ts
@@ -1,11 +1,17 @@
 import { google } from "googleapis";
 
+const credentials = JSON.parse(
+  process.env.GOOGLE_SERVICE_ACCOUNT!
+);
+
 export const auth = new google.auth.GoogleAuth({
-    keyFile:"credentials/service-account.json",
-    scopes:["https://www.googleapis.com/auth/spreadsheets"]
+  credentials,
+  scopes: [
+    "https://www.googleapis.com/auth/spreadsheets"
+  ],
 });
 
 export const sheets = google.sheets({
-    version:"v4",
-    auth,
-})
+  version: "v4",
+  auth,
+});
